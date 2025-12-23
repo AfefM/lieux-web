d9157a7a9c544ac84dd86c11aa68493814838fa2
Afef M. <afefmosbeh3@gmail.com>
2025-12-23 21:48:53 +0100

    Final fix for redirect and layout

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,15 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/fr',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
