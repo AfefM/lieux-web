a59c8ce74f79d4dc6e16b5a6cf59aa14ab6f2891
Afef M. <afefmosbeh3@gmail.com>
2025-12-23 20:38:59 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,20 +4,32 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
 
-  // 1. On laisse passer TOUS les fichiers avec une extension (images, favicons, etc.)
+  // 1. On laisse passer les fichiers avec une extension (images, logo, etc.)
   if (pathname.includes('.')) {
     return NextResponse.next()
   }
 
-  // 2. Si on est à la racine exacte "/" -> on redirige vers "/fr"
+  // 2. On définit les langues
+  const locales = ['fr', 'ar', 'en']
+
+  // 3. Si on est sur la racine "/" on va vers "/fr"
   if (pathname === '/') {
     return NextResponse.redirect(new URL('/fr', request.url))
+  }
+
+  // 4. Vérifier si l'URL commence par une langue
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  )
+
+  if (pathnameIsMissingLocale) {
+    return NextResponse.redirect(new URL(`/fr${pathname}`, request.url))
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  // On ne surveille QUE la racine et les pages, pas les dossiers système
-  matcher: ['/', '/fr', '/ar', '/en'],
+  // On cible tout sauf les dossiers système et l'api
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|public).*)'],
 }
