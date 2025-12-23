80d4ff2c008cdd73dd4e3700c1b93ea2f9571a0a
Afef M. <afefmosbeh3@gmail.com>
2025-12-23 22:52:41 +0100

    problème réglé

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
 import "./globals.css";
 
-export const metadata = {
-  title: 'Skander Zarrad | Lieux',
-  description: 'Portfolio',
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body className="bg-black antialiased text-white">
+      <body className="bg-black antialiased text-white selection:bg-white selection:text-black">
         {children}
       </body>
     </html>
